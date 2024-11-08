26ea0f5e907f68093f8c1a5a73e405341a58463d
Chen Xu <cxu.self@gmail.com>
2024-11-08 06:19:42 +0200

    Update server to use environment variable for port configuration

--- server.py
+++ server.py
@@ -52,4 +52,7 @@ def process():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Get the port from the environment variable or default to 8080
+    port = int(os.environ.get("PORT", 8080))
+
+    app.run(host='0.0.0.0', port=port)
